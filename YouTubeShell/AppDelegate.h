5178f355d36d7ff386a5854f102f234d231e2096
Vincent Chau <vichau@ucdavis.edu>
2016-09-22 18:30:49 -0700

    first commit

---
+++ YouTubeShell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YouTubeShell
+//
+//  Created by Vincent Chau on 9/22/16.
+//  Copyright © 2016 Vincent Chau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
