06d1e0424d86926527d8ada899f3cf843af97b07
Vincent Chau <vichau@ucdavis.edu>
2016-09-24 02:45:35 -0700

    added autolayout for slidemenucell

--- YouTubeShell/SlideMenuCollectionViewCell.m
+++ YouTubeShell/SlideMenuCollectionViewCell.m
@@ -7,10 +7,12 @@
 //
 
 #import "SlideMenuCollectionViewCell.h"
+#import "UIView+Constraints.h"
 
 @interface SlideMenuCollectionViewCell()
 
 @property (strong, nonatomic) UILabel *settingsNameLabel;
+@property (strong, nonatomic) UIImageView *imageView;
 
 @end
 
@@ -27,6 +29,19 @@
     return _settingsNameLabel;
 }
 
+- (UIImageView *)imageView
+{
+    if (!_imageView)
+    {
+        _imageView = [[UIImageView alloc] init];
+        _imageView.image = [[UIImage imageNamed:@"settings_btn"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        [_imageView setTintColor:[UIColor grayColor]];
+        _imageView.contentMode = UIViewContentModeScaleAspectFill;
+        _imageView.clipsToBounds = true;
+    }
+    
+    return _imageView;
+}
 - (instancetype)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -53,7 +68,21 @@
 
 - (void)setupView
 {
-    [self addSubview:_settingsNameLabel];
+    [self addSubview:self.settingsNameLabel];
+    [self addSubview:self.imageView];
+    
+    [self addVisualConstraintWithFormat:@"H:|-16-[v0(30)]-16-[v1]|" andView:@[self.imageView, self.settingsNameLabel]];
+    [self addVisualConstraintWithFormat:@"V:|[v0]|" andView:@[self.settingsNameLabel]];
+    [self addVisualConstraintWithFormat:@"V:[v0(30)]" andView:@[self.imageView]];
+    
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.imageView
+                                                     attribute:NSLayoutAttributeTop
+                                                     relatedBy:NSLayoutRelationEqual
+                                                        toItem:self
+                                                     attribute:NSLayoutAttributeTop
+                                                    multiplier:1.0
+                                                      constant:10.0]];
+    
 }
 
 @end
